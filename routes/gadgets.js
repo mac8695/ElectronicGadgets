0b0609ed7a84684160becc60067674b6d6068a28
mac8695 <Mahesh.Chavan@MyGeorgian.ca>
2016-11-22 21:06:33 -0500

    Added edit gadget page and made changes in gadget.js

--- routes/gadgets.js
+++ routes/gadgets.js
@@ -56,18 +56,14 @@ router.post('/add',function (req, res, next) {
 
             });
     });
-// GET /gaddgets/delete._id - to get delete id
+// GET /gadgets/delete._id - to get delete id
 router.get('/delete/:_id', function(req, res, next)
     {
         // get the id from the url
         var _id = req.param._id;
 
         //delete the data with that id
-        Gadget.remove(
-            {
-                _id:_id
-            },
-            function (err)
+        Gadget.remove( { _id: _id }, function (err)
             {
                 if(err)
                 {
@@ -83,7 +79,64 @@ router.get('/delete/:_id', function(req, res, next)
                 res.redirect('/gadgets');
             }
     });
-    });
+});
 
+// GET /gadgets/_id - Display the edit page with pre-filled values
+router.get('/:_id', function (req, res, next)
+{
+    //get id of selected gadget from url
+    var _id = req.params._id;
+
+    //using mongoose schema to find gadget
+    Gadget.findById({_id:_id}, function (err, gadget)
+    {
+        if (err)
+        {
+            console.log(err);
+            res.render('error', {
+                message: 'Could not load gadget',
+                error : err
+        });
+        }
+        else
+        {
+            res.render('editGadget',
+                {
+                    title: 'Edit a Gadget',
+                    gadget: gadget
+                });
+        }
+        
+    });
+});
+//POST /gadgets/_id - process changes made in form and save it
+router.post('/:_id', function (res, req, next) {
+    // get id of edited from the url
+    var _id = req.params._id;
+
+    //save the edited values in schema
+    var gadget = new Gadget({
+        _id: _id,
+        gadget: req.body.gadget,
+        typeOfGadget : req.body.typeOfGadget,
+        price: req.body.price,
+        condition: req.body.condition
+    });
+    //updated view
+    Gadget.update({_id: id}, gadget, function (err) {
+        if(err)
+        {
+            console.log(err);
+            res.render('error',{
+                message: 'Could not update Gadget',
+                error: err
+            });
+        }
+        else
+        {
+            res.render('/gadget');
+        }
+    });
+});
 //making it public
 module.exports = router;
